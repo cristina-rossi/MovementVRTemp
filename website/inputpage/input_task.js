c0ccb1618964649ff6a23d519f01a31d1c024d0a
rc-cristinarossi <90429880+rc-cristinarossi@users.noreply.github.com>
2023-10-11 14:31:10 -0400

    Update input_task.js

--- website/inputpage/input_task.js
+++ website/inputpage/input_task.js
@@ -6,14 +6,16 @@ document.querySelector('#INPUT_TASK').insertAdjacentHTML('beforeend', `
      		    <!-- SPATIAL VARIABLES -->
      		    <!-- plate position (wrt headset eye level) -->
 		    <div class="form-control">        <!-- numbers  -->
-			<strong>Plate Position [meters] &ensp; </strong>
+			<blck><strong>Plate Position [meters] &ensp; </strong>
+   			<note>(a wrt headset position - eye level)</note> </blck>
+      <blck>
    			<label for="PLATE_X">X:</label>
 			<input id="PLATE_X" name="setupScript,platePositionX" type="number" value="0" />
 			<label for="PLATE_Y">Y:</label>
 			<input id="PLATE_Y" name="setupScript,platePositionY" type="number" value="-0.3" />
 			<label for="PLATE_Z">Z:</label>
 			<input id="PLATE_Z" name="setupScript,platePositionZ" type="number" value="0.4" />
-   			<note>(a wrt headset position - eye level)</note> 
+   </blck>
 		    </div>
       
      		    <!-- target position (wrt plate start position)  -->
