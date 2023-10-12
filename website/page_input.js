345ca80b4f8be62f064a89dd63bdf8ec06ff2ff4
rc-cristinarossi <90429880+rc-cristinarossi@users.noreply.github.com>
2023-10-12 17:27:49 -0400

    Update page_input.js

--- website/page_input.js
+++ website/page_input.js
@@ -127,6 +127,12 @@ document.querySelector('#INPUT_STYLE').insertAdjacentHTML('beforeend', `
 		  border: 1px solid #90EE90;
       		} 
 	
+		.tabcontent inputcolumn name :before
+		{
+		    content: '\00000a';
+		    display: block;  
+		}
+	
 		.tabcontent label {
                   padding-right: 3px;
       		  padding-left:10px;
