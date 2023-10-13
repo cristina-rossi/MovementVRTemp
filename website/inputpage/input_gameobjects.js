236f57e25c72afae9b6ef6a1c560f65892cd3377
rc-cristinarossi <90429880+rc-cristinarossi@users.noreply.github.com>
2023-10-13 11:21:46 -0400

    Update input_gameobjects.js

--- website/inputpage/input_gameobjects.js
+++ website/inputpage/input_gameobjects.js
@@ -6,7 +6,7 @@ document.querySelector('#INPUT_GAMEOBJECTS').insertAdjacentHTML('beforeend', `
 		<inputcolumn>
   
   
-  		<!-- Stage 0 -->
+  		<!-- Plate -->
     		  <inputdivider> Plate </inputdivider>
 		  <div class="form-control">        
 			<name>Plate scale [relative]:</name>
@@ -33,7 +33,61 @@ document.querySelector('#INPUT_GAMEOBJECTS').insertAdjacentHTML('beforeend', `
 			<input id="PLATE_FRICTIOND" name="setupScript,frictionPlateHandsDynamic" type="number" value="0.6" />
 			<label for="PLATE_FRICTIONS">Static coefficient:</label>
 			<input id="PLATE_FRICTIONS" name="setupScript,frictionPlateHandsStatic" type="number" value="0.6" />
+   			</inputs>              
+			<name>Plate apperance adjustments [meters]</name>
+   			<inputs>
+   			<label for="PLATE_BUFFER">Vertical offset:</label>
+			<input id="PLATE_BUFFER" name="setupScript,plateStandYBuffer" type="number" value="0.006" />
+   			<label for="PLATE_ADJUSTTHRESH">Threshold for origin update:</label>
+			<input id="PLATE_ADJUSTTHRESH" name="setupScript,maxDistanceResetPlateOrigin" type="number" value="0.01" />
+   		  	<note>Plate is enabled (but invisible) at adjusted position to avoid collision with stand.</note> 
+   		  	<note>Plate returns to start position on stand (from Task tab) before it is set to visible.</note>
    			</inputs>   
+		   </div>
+
+     	<!-- Grape -->
+    		  <inputdivider> Grape </inputdivider>
+		  <div class="form-control">     
+			<name>Grape position offset wrt plate [meters]:</name>
+   			<inputs>
+   			<label for="GRAPE_OFFSET_X">X:</label>
+			<input id="GRAPE_OFFSET_X" name="setupScript,grapePositionX" type="number" value="0" />
+			<label for="GRAPE_OFFSET_Y">Y:</label>
+			<input id="GRAPE_OFFSET_Y" name="setupScript,grapePositionY" type="number" value="0.04" />
+			<label for="GRAPE_OFFSET_Z">Z:</label>
+			<input id="GRAPE_OFFSET_Z" name="setupScript,grapePositionZ" type="number" value="0" />
+   			</inputs>         
+			<name>Grape scale [relative]:</name>
+   			<inputs>
+   			<label for="GRAPE_SCALE_X">X:</label>
+			<input id="GRAPE_SCALE_X" name="setupScript,grapeScaleX" type="number" value="1" />
+			<label for="GRAPE_SCALE_Y">Y:</label>
+			<input id="GRAPE_SCALE_Y" name="setupScript,grapeScaleY" type="number" value="1" />
+			<label for="GRAPE_SCALE_Z">Z:</label>
+			<input id="GRAPE_SCALE_Z" name="setupScript,grapeScaleZ" type="number" value="1" />
+   			</inputs>         
+			<name>Grape physics:</name>
+   			<inputs>
+   			<label for="GRAPE_MASS">Mass [kg]:</label>
+			<input id="GRAPE_MASS" name="setupScript,grapeMass" type="number" value="1" />
+			<label for="GRAPE_DRAG">Y:</label>
+			<input id="GRAPE_DRAG" name="setupScript,grapeDrag" type="number" value="0" />
+			<label for="GRAPE_ADRAG">Angular drag:</label>
+			<input id="GRAPE_ADRAG" name="setupScript,grapeAngularDrag" type="number" value="0.05" />
+   			</inputs>             
+			<name>Grape-plate friction:</name>
+   			<inputs>
+   			<label for="GRAPE_FRICTIOND">Dynamic coefficient:</label>
+			<input id="GRAPE_FRICTIOND" name="setupScript,frictionPlateGrapeDynamic" type="number" value="0.3" />
+			<label for="GRAPE_FRICTIONS">Static coefficient:</label>
+			<input id="GRAPE_FRICTIONS" name="setupScript,frictionPlateGrapeStatic" type="number" value="0.3" />
+   			</inputs>          
+			<name>Ignore grape-hand collision:</name>
+   			<inputs>		    
+			<input id="GRAPE_HANDS_IGNORE_H" name="grapeScript,grapeAlwaysIgnoreHands" type="hidden" value="FALSE">
+			<input id="GRAPE_HANDS_IGNORE" name="grapeScript,grapeAlwaysIgnoreHands" type="checkbox" value="TRUE" >
+			<label for="GRAPE_HANDS_IGNORE"></label>	
+   			</inputs>    			
 		   </div>
   	
 	</inputcolumn> 
