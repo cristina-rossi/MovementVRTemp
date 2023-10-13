a5ffce6bf1bf217264e254cefe48854c807dcaa4
rc-cristinarossi <90429880+rc-cristinarossi@users.noreply.github.com>
2023-10-13 12:29:22 -0400

    Update input_phases.js

--- website/inputpage/input_phases.js
+++ website/inputpage/input_phases.js
@@ -1,10 +1,69 @@
 document.querySelector('#INPUT_GAMEOBJECTS').insertAdjacentHTML('beforeend', `                                 
 
     
-		<form>
+	<form>
+	<inputcolumn>
 
-		<inputcolumn>
+	<!-- Start -->
+	  <inputdivider> Start of paradigm </inputdivider>
+	  <div class="form-control">  
+   			<note>initial delay with message at the very start of paradigm</note>
+ 			<name>Message:</name>
+   			<inputs>      
+			<label for="START_MSG"></label>
+<textarea id="START_MSG" name="messagesScript,startText" rows="2" cols="50">
+Welcome!
+(Loading...)
+</textarea>
+   			</inputs>               
+			<name>Message position [meters]:</name>
+   			<inputs>
+   			<label for="STARTMSG_POS_X">X:</label>
+			<input id="STARTMSG_POS_X" name="setupScript,startMessageLocalPositionX" type="number" value="0" />
+			<label for="STARTMSG_POS_Y">Y:</label>
+			<input id="STARTMSG_POS_Y" name="setupScript,startMessageLocalPositionY" type="number" value="1.3" />
+			<label for="STARTMSG_POS_Z">Z:</label>
+			<input id="STARTMSG_POS_Z" name="setupScript,startMessageLocalPositionZ" type="number" value="4.4" />
+   			</inputs>  			
+			<name>Message font size:</name>
+   			<inputs>
+   			<label for="STARTMSG_FONT"></label>
+			<input id="STARTMSG_FONT" name="setupScript,startMessageTextSize" type="number" value="250" />
+   			</inputs>  
+			<name>Duration [seconds]:</name>
+   			<inputs>
+   			<label for="STARTMSG_TIME"></label>
+			<input id="STARTMSG_TIME" name="startScript,experimentStartTimeDelay" type="number" value="5" />
+   			</inputs>  
+	  </div>
   
+  	<!-- End -->
+	  <inputdivider> End of paradigm </inputdivider>
+	  <div class="form-control">  
+   <note>Message at the very end of paradigm</note>
+ 			<name>Message:</name>
+   			<inputs>      
+			<label for="END_MSG"></label>
+<textarea id="END_MSG" name="messagesScript,endText" rows="2" cols="50">
+The end
+</textarea>
+   			</inputs>               
+			<name>Message position [meters]:</name>
+   			<inputs>
+   			<label for="ENDMSG_POS_X">X:</label>
+			<input id="ENDMSG_POS_X" name="setupScript,endMessageLocalPositionX" type="number" value="0" />
+			<label for="ENDMSG_POS_Y">Y:</label>
+			<input id="ENDMSG_POS_Y" name="setupScript,endMessageLocalPositionY" type="number" value="0.8" />
+			<label for="ENDMSG_POS_Z">Z:</label>
+			<input id="ENDMSG_POS_Z" name="setupScript,endMessageLocalPositionZ" type="number" value="4.4" />
+   			</inputs>  			
+			<name>Message font size:</name>
+   			<inputs>
+   			<label for="ENDMSG_FONT"></label>
+			<input id="ENDMSG_FONT" name="setupScript,endMessageTextSize" type="number" value="250" />
+   			</inputs>  
+   	  </div>
+      
   	
 	</inputcolumn> 
 	<figurecolumn>
