2566dadf2eb3db513276cac369d7f3bc5f5e51c1
rc-cristinarossi <90429880+rc-cristinarossi@users.noreply.github.com>
2023-10-13 08:34:41 -0400

    Create input_phases.js

---
+++ website/inputpage/input_phases.js
@@ -0,0 +1,21 @@
+document.querySelector('#INPUT_GAMEOBJECTS').insertAdjacentHTML('beforeend', `                                 
+
+    
+		<form>
+
+		<inputcolumn>
+  
+  	
+	</inputcolumn> 
+	<figurecolumn>
+    		<img src="website/inputpage/paradigmSmall-01.png"  style="width:100%">
+	</figurecolumn>
+
+ 
+	</form>
+
+     
+  
+      
+
+  `);
