65bb6374c08e3e27a33f68d4d4a5f59c4f69727e
rc-cristinarossi <90429880+rc-cristinarossi@users.noreply.github.com>
2023-10-13 12:31:33 -0400

    Update input_phases.js

--- website/inputpage/input_phases.js
+++ website/inputpage/input_phases.js
@@ -63,6 +63,40 @@ The end
 			<input id="ENDMSG_FONT" name="setupScript,endMessageTextSize" type="number" value="250" />
    			</inputs>  
    	  </div>
+
+
+	<!-- Position hands in hand-target -->
+	  <inputdivider> "Position hands in hand-target" phase </inputdivider>
+	  <div class="form-control">  
+   			<note>asks participant to place hands within target hand-meshes to start trial</note>
+ 			<name>Message:</name>
+   			<inputs>      
+			<label for="HANDTARGET_MSG"></label>
+<textarea id="HANDTARGET_MSG" name="messagesScript,startText" rows="2" cols="50">
+Welcome!
+(Loading...)
+</textarea>
+   			</inputs>               
+			<name>Message position [meters]:</name>
+   			<inputs>
+   			<label for="HANDTARGETMSG_POS_X">X:</label>
+			<input id="HANDTARGETMSG_POS_X" name="setupScript,startMessageLocalPositionX" type="number" value="0" />
+			<label for="HANDTARGETMSG_POS_Y">Y:</label>
+			<input id="HANDTARGETMSG_POS_Y" name="setupScript,startMessageLocalPositionY" type="number" value="1.3" />
+			<label for="HANDTARGETMSG_POS_Z">Z:</label>
+			<input id="HANDTARGETMSG_POS_Z" name="setupScript,startMessageLocalPositionZ" type="number" value="4.4" />
+   			</inputs>  			
+			<name>Message font size:</name>
+   			<inputs>
+   			<label for="HANDTARGETMSG_FONT"></label>
+			<input id="HANDTARGETMSG_FONT" name="setupScript,startMessageTextSize" type="number" value="250" />
+   			</inputs>  
+			<name>Duration [seconds]:</name>
+   			<inputs>
+   			<label for="HANDTARGETMSG_TIME"></label>
+			<input id="HANDTARGETMSG_TIME" name="startScript,experimentStartTimeDelay" type="number" value="5" />
+   			</inputs>  
+	  </div>
       
   	
 	</inputcolumn> 
