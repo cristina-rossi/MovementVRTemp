aca8ea0314f7f7355ba1c2253370e624857a9308
rc-cristinarossi <90429880+rc-cristinarossi@users.noreply.github.com>
2023-10-13 08:27:17 -0400

    Update input_feedback.js

--- website/inputpage/input_feedback.js
+++ website/inputpage/input_feedback.js
@@ -80,174 +80,48 @@ document.querySelector('#INPUT_FEEDBACK').insertAdjacentHTML('beforeend', `
    			<inputs>      
 			<label for="FAIL4"></label>
 			<textarea id="FAIL4" name="messagesScript,failText4" rows="1" cols="150" value="too slow"></textarea>
-   			</inputs>      
-		   </div>
-     
-     
-  		<!-- Stage 1 -->
-    		  <inputdivider> Stage 1: "place hands in hand-target" phase </inputdivider>
-   		  <note>Only present in selected trials - defined in "Phases" tab </note> 
-    		
-  		<!-- Stage 2 -->
-    		  <inputdivider> Stage 2: reposition hands to enable plate </inputdivider>
-		  <div class="form-control">           
-   		  	<note>No plate yet. Waits for hands to outside of off-limits region: </note> 
-			<name>Off-limits region center [meters]</name>
+   			</inputs>    
+              
+			<name>Message position [meters]:</name>
    			<inputs>
-   			<label for="OFFLIMITS_POS_X">X:</label>
-			<input id="OFFLIMITS_POS_X" name="setupScript,stage2_offlimitsPositionX" type="number" value="0" />
-			<label for="OFFLIMITS_POS_Y">Y:</label>
-			<input id="OFFLIMITS_POS_Y" name="setupScript,stage2_offlimitsPositionY" type="number" value="0" />
-			<label for="OFFLIMITS_POS_Z">Z:</label>
-			<input id="OFFLIMITS_POS_Z" name="setupScript,stage2_offlimitsPositionZ" type="number" value="0.4" />
-   			</inputs>       
-			<name>Off-limits region size [meters]</name>
+   			<label for="FAIL_POS_X">X:</label>
+			<input id="FAIL_POS_X" name="setupScript,failMessageLocalPositionX" type="number" value="0.4" />
+			<label for="FAIL_POS_Y">Y:</label>
+			<input id="FAIL_POS_Y" name="setupScript,failMessageLocalPositionY" type="number" value="0.15" />
+			<label for="FAIL_POS_Z">Z:</label>
+			<input id="FAIL_POS_Z" name="setupScript,failMessageLocalPositionZ" type="number" value="0.65" />
+   			</inputs>  
+			<name>Message size [relative]:</name>
    			<inputs>
-   			<label for="OFFLIMITS_SIZE_X">X:</label>
-			<input id="OFFLIMITS_SIZE_X" name="setupScript,stage2_offlimitsPositionX" type="number" value="2" />
-			<label for="OFFLIMITS_SIZE_Y">Y:</label>
-			<input id="OFFLIMITS_SIZE_Y" name="setupScript,stage2_offlimitsPositionY" type="number" value="0.62" />
-			<label for="OFFLIMITS_SIZE_Z">Z:</label>
-			<input id="OFFLIMITS_SIZE_Z" name="setupScript,stage2_offlimitsPositionZ" type="number" value="2" />
-   			</inputs>           
-   		  	<note>Proceeds after hands are outside of region for a certain time delay: </note> 
-			<name>Time to plate activation [seconds]:</name>
-   			<inputs>
-   			<label for="OFFLIMITS_TIME_ACTIVATEPLATE">to activate plate (invisible):</label>
-			<input id="OFFLIMITS_TIME_ACTIVATEPLATE" name="trialScript,stage2_handsRepositionedActivateTime" type="number" value="0.05" />
-   			</inputs>
-			<name>Time to next stage [seconds]:</name>
-   			<inputs>
-   			<label for="OFFLIMITS_TIME_TOT">to proceed to next stage (plate visible):</label>
-			<input id="OFFLIMITS_TIME_TOT" name="trialScript,stage2_handsRepositionedTotalTime" type="number" value="0.1" />
-   			</inputs>
-		   </div>
-     
-   		  <note>Plate appears at this position:</note> 
-     		    <!-- plate position (wrt headset eye level) -->      
-		    <div class="form-control">        <!-- numbers  -->
-			<name>Plate position [meters]</name>
-   			<inputs>
-   			<label for="PLATE_X">X:</label>
-			<input id="PLATE_X" name="setupScript,platePositionX" type="number" value="0" />
-			<label for="PLATE_Y">Y:</label>
-			<input id="PLATE_Y" name="setupScript,platePositionY" type="number" value="-0.3" />
-			<label for="PLATE_Z">Z:</label>
-			<input id="PLATE_Z" name="setupScript,platePositionZ" type="number" value="0.4" />
-   			</inputs>
-		    </div>
-     
-  		<!-- Stage 3 -->
-    		  <inputdivider> Stage 3: make contact with plate </inputdivider>
-   		  <note>Plate active and visible - waits & detects contact between hands and plate.</note> 
-   		  <note>Skips to next trial (failure) if nothing happens after time limit: </note> 
-		  <div class="form-control">        
-			<name>Time limit [seconds]:</name>
-   			<inputs>
-   			<label for="STAGE3_TIME"></label>
-			<input id="STAGE3_TIME" name="trialScript,stage3_maxTime" type="number" value="30" />
-   			</inputs>      
-		   </div>
-
-     
-  		<!-- Stage 4 SUCCESS-->
-    		  <inputdivider> Stage 4: moving the plate to the target - Success criteria</inputdivider>	
-   		  <note>For a success, plate must reach "target position":</note> 
-      
-     		    <!-- target position (wrt headset eye center)  -->
-		    <div class="form-control">        <!-- numbers  -->
-			<name>Target position [meters]</name>
-   			<inputs>
-			<label for="TARGET_X">X:</label>
-			<input id="TARGET_X" name="setupScript,targetPositionX" type="number" value="0" />
-			<label for="TARGET_Y">Y:</label>
-			<input id="TARGET_Y" name="setupScript,targetPositionY" type="number" value="0" />
-			<label for="TARGET_Z">Z:</label>
-			<input id="TARGET_Z" name="setupScript,targetPositionZ" type="number" value="0.4" />
-   			</inputs>
-		    </div>
-      
-   		    <note>Specifically, the distance between plate center and target position must be less than "target size":</note> 
-     		    <!-- target collider size  -->
-		    <div class="form-control">        <!-- numbers  -->     
-			<name>Target size [meters]</name>
-   			<inputs> 
-			<label for="TARGETSIZE_X">X:</label>
-			<input id="TARGETSIZE_X" name="setupScript,targetColliderSizeX" type="number" value="0.1" />
-			<label for="TARGETSIZE_Y">Y:</label>
-			<input id="TARGETSIZE_Y" name="setupScript,targetColliderSizeY" type="number" value="0.1" />
-			<label for="TARGETSIZE_Z">Z:</label>
-			<input id="TARGETSIZE_Z" name="setupScript,targetColliderSizeZ" type="number" value="0.1" />   
-   			</inputs>
-		    </div>
-      
-      
-   		    <note>The plate tilt/rotation must also be less than:</note> 
-     		    <!-- max plate tilt for success -->
-		    <div class="form-control">        <!-- numbers  -->
-			<name>Max plate tilt at target [deg]</name>
-   			<inputs> 
-			<label for="MAXTILT_X">X (front-back):</label>
-			<input id="MAXTILT_X" name="plateScript,maxTiltForSuccessX" type="number" value="180" />
-			<label for="MAXTILT_Y">Y (flat rotation):</label>
-			<input id="MAXTILT_Y" name="plateScript,maxTiltForSuccessY" type="number" value="180" />
-			<label for="MAXTILT_Z">Z (left-right):</label>
-			<input id="MAXTILT_Z" name="plateScript,maxTiltForSuccessZ" type="number" value="10" />
-   			</inputs>
-		    </div>
-      
-   		    <note>For a success, these conditions must remain true for:</note> 
-		    <div class="form-control">    
-			<name>Time in target for success [seconds]</name>
-			<inputs>
-			<label for="TIME_IN_TARGET"></label>
-			<input id="TIME_IN_TARGET" name="plateScript,timeInTargetForSuccess" type="number" value="0.01" />
-			</inputs>
-		    </div>
-	 
-    		  <inputdivider> Stage 4: moving the plate to the target - Failure criteria</inputdivider>	
-   		  <note>Trial fails for any of the following conditions:</note> 
-      		  <note>If plate reaches target position and it is too tilted. </note> 
-      		  <note>If plate reaches target position with acceptable tilt, but exits target region or becomes too titled before the "time in target for success"</note> 
-      		  <note>If plate reaches target position with acceptable tilt, but exits target region or becomes too titled before the "time in target for success"</note> 	  
-      		  <note>If plate exits the game area (falls/thrown...):</note> 
-    
-    		    <div class="form-control">        
-			<name>Game area center [meters]</name>
-   			<inputs>
-   			<label for="GAMEAREA_POS_X">X:</label>
-			<input id="GAMEAREA_POS_X" name="setupScript,gameareaPositionX" type="number" value="0" />
-			<label for="GAMEAREA_POS_Y">Y:</label>
-			<input id="GAMEAREA_POS_Y" name="setupScript,gameareaPositionY" type="number" value="0" />
-			<label for="GAMEAREA_POS_Z">Z:</label>
-			<input id="GAMEAREA_POS_Z" name="setupScript,gameareaPositionZ" type="number" value="0.4" />
-   			</inputs>       
-			<name>Game area size [meters]</name>
-   			<inputs>
-   			<label for="GAMEAREA_SIZE_X">X:</label>
-			<input id="GAMEAREA_SIZE_X" name="setupScript,gameareaSizeX" type="number" value="1" />
-			<label for="GAMEAREA_SIZE_Y">Y:</label>
-			<input id="GAMEAREA_SIZE_Y" name="setupScript,gameareaSizeX" type="number" value="1" />
-			<label for="GAMEAREA_SIZE_Z">Z:</label>
-			<input id="GAMEAREA_SIZE_Z" name="setupScript,gameareaSizeX" type="number" value="1" />
-   			</inputs>     
-
-			<name> </name>  
-   			<inputs>	    
-			<input id="GAMEAREA_BOTTOMAUTO_H" name="setupScript,gameareaBottomIsGround" type="hidden" value="FALSE">
-			<input id="GAMEAREA_BOTTOMAUTO" name="setupScript,gameareaBottomIsGround" type="checkbox" value="TRUE" checked>
-			<label for="GAMEAREA_BOTTOMAUTO">Make gamearea bottom equal to ground</label>		
+   			<label for="FAIL_SIZE_X">X:</label>
+			<input id="FAIL_SIZE_X" name="setupScript,failMessageScaleX" type="number" value="20" />
+			<label for="FAIL_SIZE_Y">Y:</label>
+			<input id="FAIL_SIZE_Y" name="setupScript,failMessageScaleY" type="number" value="30" />
    			</inputs>   
-		   </div>  
-     
-      		  <note>After time limit, past which trial is skipped:</note> 
-		  <div class="form-control">            
-			<name>Time limit [seconds]:</name>
+			<name>Message font size:</name>
    			<inputs>
-			<input id="STAGE4_TIME" name="trialScript,stage4_maxTime" type="number" value="35" />
-   			<label for="STAGE4_TIME"></label>
-   			</inputs>      
+   			<label for="FAIL_FONT"></label>
+			<input id="FAIL_FONT" name="setupScript,failMessageTextSize" type="number" value="50" />
+   			</inputs>  
+			<name>Message duration [seconds]:</name>
+   			<inputs>
+   			<label for="FAIL_TIME"></label>
+			<input id="FAIL_TIME" name="trialScript,stage5_outcomeMessageDuration" type="number" value="1" />
+   			</inputs>  
 		   </div>
+
+     
+  		<!-- bird sound -->
+    		  <inputdivider> Bird sound </inputdivider>
+		  <div class="form-control">        
+			<name>Volume [0 to 1]:</name>
+   			<inputs>      
+			<label for="BIRDVOL"></label>
+			<input id="BIRDVOL" name="setupScript,targetBirdVolume" type="number" value="1" />
+   			</inputs>      
+   		        <note>Change sounds by replacing failure and success mp3 files in Resources</note>    
+		   </div>
+     
 
 
 	</inputcolumn> 
